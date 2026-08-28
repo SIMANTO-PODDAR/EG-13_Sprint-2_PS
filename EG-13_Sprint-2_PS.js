512843ae88765821082f53c89d13448ff0e7f0b5
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-08-28 15:22:03 +0600

    Reverse a String

---
+++ EG-13_Sprint-2_PS.js
@@ -0,0 +1,63 @@
+// 1. Reverse a String 
+function reverseString(str) {
+    let revstr = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        revstr = revstr + str[i]
+    }
+    return revstr
+}
+
+
+
+// 2. Find Maximum 
+function findMax(nums) {
+}
+
+
+
+// 3. Check for Palindrome 
+function isPalindrome(str) {
+}
+
+
+
+// 4. Sum Array Elements 
+function sumArray(nums) {
+}
+
+
+
+// 5. Count Vowels 
+function countVowels(str) {
+}
+
+
+
+// 6. Two Sum 
+function twoSum(nums, target) {
+}
+
+
+
+// 7. Flatten a Nested Array 
+function flattenArray(arr) {
+}
+
+
+
+// 8. Group Anagrams 
+function groupAnagrams(strs) {
+}
+
+
+
+// 9. Longest Substring Without Repeating Characters 
+function lengthOfLongestSubstring(s) {
+}
+
+
+
+// 10. Deep Clone an Object 
+function deepClone(obj) {
+}
